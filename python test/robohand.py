16f8940e5317721af3e58b5f3cb5cc68eee8ede9
sudeepgupta13 <sudeepgupta13@gmail.com>
2025-08-27 18:22:25 +0530

    w

--- python test/robohand.py
+++ python test/robohand.py
@@ -1,2 +1,21 @@
+def get_gripping_force(object_weight_kg):
+    """
+    Determines the optimal gripping force for a robotic arm.
+    Args:
+        object_weight_kg (float): The weight of the object in kilograms.
+    Returns:
+        str: Description of the gripping force.
+    """
+    if object_weight_kg < 0.5:
+        return "Light_Grip"
+    elif 0.5 <= object_weight_kg < 2.0:
+        return "Medium_Grip"
+    else:
+        return "Strong_Grip"
+
+# Usage
+item_weight = 4
+grip_strength = get_gripping_force(item_weight)
+print(f"Required gripping force: {grip_strength}")
 
 
